3e0942f7441d1f0a806c1c967dc0610a9be1662a
Nikki Rufiansya <nikkirufiansya88@gmail.com>
2025-01-29 14:38:19 +0700

    add jailmonkey

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,29 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, Alert } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useEffect } from 'react';
+import JailMonkey from 'jail-monkey';
 
 export default function HomeScreen() {
+
+  useEffect(() => {
+    try {
+      if (JailMonkey?.isJailBroken?.()) {
+        Alert.alert(
+          'Security Warning',
+          'This device is jailbroken/rooted. The app may not function properly.',
+          [{ text: 'OK' }]
+        );
+      }
+    } catch (error) {
+      console.warn('JailMonkey module is not available:', error);
+    }
+  }, []);
+
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -16,7 +34,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome Testing Jailmonkey!</ThemedText>
+        <ThemedText type="title">Welcome Jailmonkey!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
